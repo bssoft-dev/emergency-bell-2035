e540c494e2cb35631c090752e8ea0b12fc0e4d84
bssoft <bsjang@bs-soft.co.kr>
2022-03-22 11:46:20 +0900

    edit dashboard websocket

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -33,21 +33,17 @@ export class DashboardComponent implements OnInit {
         }
     }
 
-    //api 데이터
-    detectionstatusdata = [];
-    alldetectiondata = [];
 
-    //all디바이스
-    numdevice: any;
-    deactivatedevice: any;
-    inspectiondevice: any;
-    activatedevice: any;
-
-    //활성화체크
-    alivecheckdata: any[] = [];
-    alivecheckdatakey: string[] = [];
-    alivecheckdatavalue: string[] = [];
-    tempdata: any[] = [];
+    // detectionstatusdata = [];
+    // alldetectiondata = [];
+    // numdevice: any;
+    // deactivatedevice: any;
+    // inspectiondevice: any;
+    // activatedevice: any;
+    // alivecheckdata: any[] = [];
+    // alivecheckdatakey: string[] = [];
+    // alivecheckdatavalue: string[] = [];
+    // tempdata: any[] = [];
 
 
 
@@ -88,14 +84,13 @@ export class DashboardComponent implements OnInit {
             this.socketconnectdata = this.requestreceived[0].connect.content;
             this.socketdevicesdata = this.requestreceived[0].devices.content;
             this.socketgraphdata = this.requestreceived[0].graph.content;
-
             this.sockethistorydata = this.requestreceived[0].history.content;
             this.socketrecentdata = this.requestreceived[0].recent.content;
-            console.log("Response from websocket: ", msg);
         });
     }
 
     sent = [];
+
     initrequest() {
         setTimeout(() => {
             let requestmessage = { cmd: "main", args: ["test"] }
@@ -104,222 +99,219 @@ export class DashboardComponent implements OnInit {
         }, 100)
     }
 
-
-
     makechart = (socketgraphdata) => {
         const dataset = []
-        for (let i = 0; i < this.socketgraphdata["Button"].length; i++) {
-            dataset.push({ Button: this.socketgraphdata["Button"][i], Scream: this.socketgraphdata["Scream"][i], Time: this.socketgraphdata["Time"][i] })
+        for (let i = 0; i < socketgraphdata["Button"].length; i++) {
+            dataset.push({ Button: socketgraphdata["Button"][i], Scream: socketgraphdata["Scream"][i], Time: socketgraphdata["Time"][i] })
         }
 
 
-        setTimeout(() => {
-            var root = am5.Root.new("chartdiv");
+        var root = am5.Root.new("chartdiv");
 
 
-            root.setThemes([
-                am5themes_Animated.new(root)
-            ]);
+        root.setThemes([
+            am5themes_Animated.new(root)
+        ]);
 
-            root.dateFormatter.setAll({
-                dateFormat: "yyyy-MM-dd",
-                dateFields: ["valueX"]
-            });
+        root.dateFormatter.setAll({
+            dateFormat: "yyyy-MM-dd",
+            dateFields: ["valueX"]
+        });
 
-            // Create chart
-            // https://www.amcharts.com/docs/v5/charts/xy-chart/
-            var chart = root.container.children.push(am5xy.XYChart.new(root, {
-                focusable: true,
-                panX: false,
-                panY: false,
-                wheelY: "zoomX",
-                layout: root.verticalLayout
-            }));
+        // Create chart
+        // https://www.amcharts.com/docs/v5/charts/xy-chart/
+        var chart = root.container.children.push(am5xy.XYChart.new(root, {
+            focusable: true,
+            panX: false,
+            panY: false,
+            wheelY: "zoomX",
+            layout: root.verticalLayout
+        }));
 
-            var data =
-                // [{ Button: 14, Scream: 15, Time: '2020-02-01' }, { Button: 20, Scream: 15, Time: '2020-02-02' }, { Button: 7, Scream: 10, Time: '201909' }, { Button: 1, Scream: 4, Time: '201908' }];
-                dataset;
-
-
-
-            // Create axes
-            // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-            var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
-                categoryField: "Time",
-                renderer: am5xy.AxisRendererX.new(root, {}),
-                tooltip: am5.Tooltip.new(root, {})
-            }));
-
-            xAxis.data.setAll(data);
+        var data =
+            // [{ Button: 14, Scream: 15, Time: '2020-02-01' }, { Button: 20, Scream: 15, Time: '2020-02-02' }, { Button: 7, Scream: 10, Time: '201909' }, { Button: 1, Scream: 4, Time: '201908' }];
+            dataset;
 
 
 
+        // Create axes
+        // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+        var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+            categoryField: "Time",
+            renderer: am5xy.AxisRendererX.new(root, {}),
+            tooltip: am5.Tooltip.new(root, {})
+        }));
 
-            var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-                min: 0,
-                renderer: am5xy.AxisRendererY.new(root, {})
-            }));
+        xAxis.data.setAll(data);
 
-            // Add legend
-            // https://www.amcharts.com/docs/v5/charts/xy-chart/legend-xy-series/
-            var legend = chart.children.push(am5.Legend.new(root, {
-                centerX: am5.p50,
-                x: am5.p50
-            }));
 
-            // set text,grid color white
-            root.interfaceColors.set("grid", am5.color("#ffffff"));
-            root.interfaceColors.set("text", am5.color("#ffffff"));
 
-            // Add series
-            // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-            function makeSeries(name: any, fieldName: any) {
-                if (name === "Button") {
-                    var series = chart.series.push(am5xy.ColumnSeries.new(root, {
-                        fill: am5.color("#6794dc"),
-                        name: name,
-                        stacked: true,
-                        xAxis: xAxis,
-                        yAxis: yAxis,
-                        valueYField: fieldName,
-                        // valueYShow: "valueYTotalPercent",
-                        categoryXField: "Time"
-                    }));
-                } else {
-                    var series = chart.series.push(am5xy.ColumnSeries.new(root, {
-                        fill: am5.color("#a367dc"),
-                        name: name,
-                        stacked: true,
-                        xAxis: xAxis,
-                        yAxis: yAxis,
-                        valueYField: fieldName,
-                        // valueYShow: "valueYTotalPercent",
-                        categoryXField: "Time"
-                    }));
-                }
-                series.columns.template.setAll({
-                    maxWidth: 50,
-                    strokeOpacity: 0,
-                    tooltipText: "{name}, {categoryX}:{valueY}",
-                    tooltipY: am5.percent(10),
-                });
-                series.data.setAll(data);
 
-                // Make stuff animate on load
-                // https://www.amcharts.com/docs/v5/concepts/animations/
-                series.appear();
+        var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+            min: 0,
+            renderer: am5xy.AxisRendererY.new(root, {})
+        }));
 
-                series.bullets.push(function () {
-                    return am5.Bullet.new(root, {
-                        sprite: am5.Label.new(root, {
-                            text: "{valueY}",
-                            fill: root.interfaceColors.get("alternativeText"),
-                            centerY: am5.p50,
-                            centerX: am5.p50,
-                            populateText: true
-                        })
-                    });
-                });
-                legend.data.push(series);
+        // Add legend
+        // https://www.amcharts.com/docs/v5/charts/xy-chart/legend-xy-series/
+        var legend = chart.children.push(am5.Legend.new(root, {
+            centerX: am5.p50,
+            x: am5.p50
+        }));
+
+        // set text,grid color white
+        root.interfaceColors.set("grid", am5.color("#ffffff"));
+        root.interfaceColors.set("text", am5.color("#ffffff"));
+
+        // Add series
+        // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+        function makeSeries(name: any, fieldName: any) {
+            if (name === "Button") {
+                var series = chart.series.push(am5xy.ColumnSeries.new(root, {
+                    fill: am5.color("#6794dc"),
+                    name: name,
+                    stacked: true,
+                    xAxis: xAxis,
+                    yAxis: yAxis,
+                    valueYField: fieldName,
+                    // valueYShow: "valueYTotalPercent",
+                    categoryXField: "Time"
+                }));
+            } else {
+                var series = chart.series.push(am5xy.ColumnSeries.new(root, {
+                    fill: am5.color("#a367dc"),
+                    name: name,
+                    stacked: true,
+                    xAxis: xAxis,
+                    yAxis: yAxis,
+                    valueYField: fieldName,
+                    // valueYShow: "valueYTotalPercent",
+                    categoryXField: "Time"
+                }));
             }
-
-
-            makeSeries("Button", "Button");
-            makeSeries("Scream", "Scream");
-
-            xAxis.get("renderer").labels.template.setAll({
-                fill: root.interfaceColors.get("alternativeText")
+            series.columns.template.setAll({
+                maxWidth: 50,
+                strokeOpacity: 0,
+                tooltipText: "{name}, {categoryX}:{valueY}",
+                tooltipY: am5.percent(10),
             });
+            series.data.setAll(data);
 
             // Make stuff animate on load
             // https://www.amcharts.com/docs/v5/concepts/animations/
-        }, 100);
-    }
+            series.appear();
 
-    detectiongraph() {
-        let detectiongraphdata: any[] = [];
-        let dataset: any[] = [];
-        this.service.detectiongraph(localStorage.getItem('customer_code')).subscribe({
-            next: (res) => {
-                detectiongraphdata.push(res)
-                const { Button, Scream, Time } = detectiongraphdata[0];
-                for (let i = 0; i < Button.length; i++) {
-                    dataset.push({ Button: Button[i], Scream: Scream[i], Time: Time[i] })
-                }
-                console.log(dataset, '그래프데이터')
-                this.makechart(dataset)
-            },
-            error: (err) => {
+            series.bullets.push(function () {
+                return am5.Bullet.new(root, {
+                    sprite: am5.Label.new(root, {
+                        text: "{valueY}",
+                        fill: root.interfaceColors.get("alternativeText"),
+                        centerY: am5.p50,
+                        centerX: am5.p50,
+                        populateText: true
+                    })
+                });
+            });
+            legend.data.push(series);
+        }
 
-            },
-            complete: () => {
-            }
+
+        makeSeries("Button", "Button");
+        makeSeries("Scream", "Scream");
+
+        xAxis.get("renderer").labels.template.setAll({
+            fill: root.interfaceColors.get("alternativeText")
         });
+
+        // Make stuff animate on load
+        // https://www.amcharts.com/docs/v5/concepts/animations/
+
     }
 
-    detectionstatus() {
-        this.service.detectionstatus(localStorage.getItem('customer_code')).subscribe({
-            next: (res) => {
-                this.detectionstatusdata.push(res)
-            },
-            error: (err) => {
+    // detectiongraph() {
+    //     let detectiongraphdata: any[] = [];
+    //     let dataset: any[] = [];
+    //     this.service.detectiongraph(localStorage.getItem('customer_code')).subscribe({
+    //         next: (res) => {
+    //             detectiongraphdata.push(res)
+    //             const { Button, Scream, Time } = detectiongraphdata[0];
+    //             for (let i = 0; i < Button.length; i++) {
+    //                 dataset.push({ Button: Button[i], Scream: Scream[i], Time: Time[i] })
+    //             }
+    //             console.log(dataset, '그래프데이터')
+    //             this.makechart(dataset)
+    //         },
+    //         error: (err) => {
 
-            },
-            complete: () => {
-            }
-        });
-    }
+    //         },
+    //         complete: () => {
+    //         }
+    //     });
+    // }
 
+    // detectionstatus() {
+    //     this.service.detectionstatus(localStorage.getItem('customer_code')).subscribe({
+    //         next: (res) => {
+    //             this.detectionstatusdata.push(res)
+    //         },
+    //         error: (err) => {
 
-    alldevice() {
-        this.service.alldevice(localStorage.getItem('customer_code')).subscribe({
-            next: (res) => {
-                this.numdevice = res.numDevice
-                this.deactivatedevice = res.deactivateDevice
-                this.inspectiondevice = res.inspectionDevice
-                this.activatedevice = (this.numdevice - this.deactivatedevice - this.inspectiondevice)
-            },
-            error: (err) => {
-
-            },
-            complete: () => {
-            }
-        });
-    }
-
-
-    alivecheck() {
-        this.service.alivecheck(localStorage.getItem('customer_code')).subscribe({
-            next: (res) => {
-                this.alivecheckdatakey = Object.keys(res);
-                this.alivecheckdatavalue = Object.values(res);
-                for (let i = 0; i < this.alivecheckdatakey.length; i++) {
-                    this.tempdata.push(this.alivecheckdatakey[i])
-                    this.tempdata.push(this.alivecheckdatavalue[i])
-                    this.alivecheckdata.push(this.tempdata);
-                    this.tempdata = [];
-                }
-            },
-            error: (err) => {
-            },
-            complete: () => {
-            }
-        });
-    }
+    //         },
+    //         complete: () => {
+    //         }
+    //     });
+    // }
 
 
-    alldetection() {
-        this.service.alldetection(localStorage.getItem('customer_code')).subscribe({
-            next: (res) => {
-                this.alldetectiondata.push(res)
-            },
-            error: (err) => {
+    // alldevice() {
+    //     this.service.alldevice(localStorage.getItem('customer_code')).subscribe({
+    //         next: (res) => {
+    //             this.numdevice = res.numDevice
+    //             this.deactivatedevice = res.deactivateDevice
+    //             this.inspectiondevice = res.inspectionDevice
+    //             this.activatedevice = (this.numdevice - this.deactivatedevice - this.inspectiondevice)
+    //         },
+    //         error: (err) => {
 
-            },
-            complete: () => {
-            }
-        });
-    }
+    //         },
+    //         complete: () => {
+    //         }
+    //     });
+    // }
+
+
+    // alivecheck() {
+    //     this.service.alivecheck(localStorage.getItem('customer_code')).subscribe({
+    //         next: (res) => {
+    //             this.alivecheckdatakey = Object.keys(res);
+    //             this.alivecheckdatavalue = Object.values(res);
+    //             for (let i = 0; i < this.alivecheckdatakey.length; i++) {
+    //                 this.tempdata.push(this.alivecheckdatakey[i])
+    //                 this.tempdata.push(this.alivecheckdatavalue[i])
+    //                 this.alivecheckdata.push(this.tempdata);
+    //                 this.tempdata = [];
+    //             }
+    //         },
+    //         error: (err) => {
+    //         },
+    //         complete: () => {
+    //         }
+    //     });
+    // }
+
+
+    // alldetection() {
+    //     this.service.alldetection(localStorage.getItem('customer_code')).subscribe({
+    //         next: (res) => {
+    //             this.alldetectiondata.push(res)
+    //         },
+    //         error: (err) => {
+
+    //         },
+    //         complete: () => {
+    //         }
+    //     });
+    // }
 
 
     ngOnInit() {
@@ -336,10 +328,6 @@ export class DashboardComponent implements OnInit {
         }, 300)
 
     }
-
-
-
-
 
     clickedModalClose() {
         this.modal = false;
