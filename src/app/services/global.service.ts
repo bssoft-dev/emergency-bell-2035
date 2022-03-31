a45d6f75648b03bb683b3c794806064454e6e8ff
bssoft <bsjang@bs-soft.co.kr>
2022-03-31 17:26:44 +0900

    edit

---
+++ src/app/services/global.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalService {
+
+  constructor() { }
+
+  private subject = new Subject<any>();
+
+  sendMessage(message: string) {
+    this.subject.next(message);
+  }
+
+  getMessage(): Observable<any> {
+    return this.subject.asObservable();
+  }
+}
