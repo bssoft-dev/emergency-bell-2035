0e7820ba3a87d3484efb8c2c5e26bb2e72af3b97
bssoft <bsjang@bs-soft.co.kr>
2022-04-19 14:52:22 +0900

    back up

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -50,20 +50,17 @@ export class DashboardComponent implements OnInit {
             this.requestreceived.push(msg);
             console.log("Response from websocket: ", msg);
 
-            if (Object.keys(msg).length >= 3) {
-                this.socketconnectdata = this.requestreceived[0]?.connect?.content;
+            if (Object.keys(msg).length > 2) {
                 this.socketdevicesdata = this.requestreceived[0]?.devices?.content;
                 this.socketgraphdata = this.requestreceived[0]?.graph?.content;
-                this.sockethistorydata = this.requestreceived[0]?.history?.content;
                 this.socketrecentdata = this.requestreceived[0]?.recent?.content;
-                this.socketmapdata = this.requestreceived[0]?.map?.content;
                 this.requestreceived = [];
             } else {
                 for (let i of this.requestreceived) {
                     if (i.title === "recent") {
                         this.socketrecentdata = i.content;
-                    } else if (i.title === "history") {
-                        this.sockethistorydata = i.content;
+                    } else if (i.title === "graph") {
+                        this.socketgraphdata = i.content;
                     } else {
                         this.popupdata = i.content;
                         if (this.popupdata) {
