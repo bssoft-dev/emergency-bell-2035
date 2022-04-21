23e0ed433eea7028b66c326a74177313c7762c5e
bssoft <bsjang@bs-soft.co.kr>
2022-04-21 15:36:47 +0900

    edit alert

--- src/app/forgotpassword/forgotpassword.component.ts
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -27,7 +27,7 @@ export class ForgotpasswordComponent implements OnInit {
     if (this.forgotpasswordForm.valid) {
       this.service.forgotpassword(data).subscribe({
         next: (res) => {
-          alert('비밀번호 초기화 URL이 문자로 발송되었습니다. 문자를 확인해주세요')
+          alert('비밀번호 초기화 URL이 문자로 발송되었습니다. 이메일을 확인해주세요')
           this.router.navigate(['/login']);
         },
         error: (err) => {
