7272224ab535c7d1cae133ad095765dd07854511
bssoft <bsjang@bs-soft.co.kr>
2022-04-28 11:03:49 +0900

    edit dashboard page

--- src/app/main/client/client.component.ts
+++ src/app/main/client/client.component.ts
@@ -35,9 +35,9 @@ export class ClientComponent implements OnInit {
     return new Promise((resolve, reject) => {
       this.service.getcurrentuser(token).subscribe({
         next: (res) => {
-          resolve(res);
           this.token = sessionStorage.getItem('token');
           this.customer_code = res.customerCode;
+          resolve(res);
         },
         error: (err) => {
           reject(new Error(err));
