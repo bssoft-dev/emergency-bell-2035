821db08dd37a0b98acc13209920dde5350f304d4
byryu <byryu@bs-soft.co.kr>
2022-06-21 01:41:03 +0000

    닉네임필수해제

--- src/app/main/device/device.component.ts
+++ src/app/main/device/device.component.ts
@@ -4,27 +4,29 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ApiService } from '../../services/api.service';
 
-
 @Component({
   selector: 'app-device',
   templateUrl: './device.component.html',
-  styleUrls: ['./device.component.css']
+  styleUrls: ['./device.component.css'],
 })
 export class DeviceComponent implements OnInit {
-
   deviceenrollForm: FormGroup;
   devicemodifyForm: FormGroup;
   public modal: boolean = false;
   public modal2: boolean = false;
   public picturemodal: boolean = false;
-  token = "";
-  customer_code = "";
+  token = '';
+  customer_code = '';
 
   fileSelected?: Blob;
   imageSrc: string;
   bigpicturesrc: string;
 
-  constructor(public router: Router, private service: ApiService, private sant: DomSanitizer) { }
+  constructor(
+    public router: Router,
+    private service: ApiService,
+    private sant: DomSanitizer
+  ) {}
 
   currentusercheckdata = [];
   currentusercheck() {
@@ -39,40 +41,34 @@ export class DeviceComponent implements OnInit {
         error: (err) => {
           reject(new Error(err));
         },
-        complete: () => {
-        }
-      })
-    })
+        complete: () => {},
+      });
+    });
   }
 
   initgetalldevices(res) {
     this.getalldevicesdata = [];
-    const temp = [sessionStorage.getItem('token'), res.customerCode]
+    const temp = [sessionStorage.getItem('token'), res.customerCode];
 
     this.service.getalldevices(temp).subscribe({
       next: (res) => {
         this.getalldevicesdata = res;
       },
-      error: (err) => {
-
-      },
-      complete: () => {
-      }
+      error: (err) => {},
+      complete: () => {},
     });
   }
 
   customerNames = [];
   initGetCustomerNames() {
-    const temp = sessionStorage.getItem('token')
+    const temp = sessionStorage.getItem('token');
     this.customerNames = [];
     this.service.getCustomerNames(temp).subscribe({
       next: (res) => {
-        this.customerNames = res
+        this.customerNames = res;
       },
-      error: (err) => {
-      },
-      complete: () => {
-      }
+      error: (err) => {},
+      complete: () => {},
     });
   }
 
@@ -80,65 +76,62 @@ export class DeviceComponent implements OnInit {
   initgetunregidevices() {
     this.service.getunregidevices().subscribe({
       next: (res) => {
-        this.unregidevices = res
-
+        this.unregidevices = res;
       },
-      error: (err) => {
-      },
-      complete: () => {
-      }
-    })
+      error: (err) => {},
+      complete: () => {},
+    });
   }
-
 
   getalldevicesdata = [];
   getonedevicedata = [];
   getalldevices() {
     this.getalldevicesdata = [];
-    const temp = [this.token, this.customer_code]
+    const temp = [this.token, this.customer_code];
 
     this.service.getalldevices(temp).subscribe({
       next: (res) => {
         this.getalldevicesdata = res;
       },
-      error: (err) => {
-
-      },
-      complete: () => {
-      }
+      error: (err) => {},
+      complete: () => {},
     });
   }
 
-
   ngOnInit(): void {
     this.currentusercheck().then((res) => {
-      this.initgetalldevices(res)
+      this.initgetalldevices(res);
       this.initGetCustomerNames();
       this.initgetunregidevices();
-    })
+    });
 
     this.deviceenrollForm = new FormGroup({
-      'deviceId': new FormControl(null, [Validators.required]),
-      'name': new FormControl("",),
-      'customerName': new FormControl(null, [Validators.required]),
-      'model': new FormControl("",),
-      'location': new FormControl("", [Validators.required, Validators.maxLength(30)]),
-      'installDate': new FormControl("", [Validators.required]),
-      'picture': new FormControl("",),
-      'communicateMethod': new FormControl("",),
-      'userMemo': new FormControl("",),
+      deviceId: new FormControl(null, [Validators.required]),
+      name: new FormControl(''),
+      customerName: new FormControl(null, [Validators.required]),
+      model: new FormControl(''),
+      location: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(30),
+      ]),
+      installDate: new FormControl('', [Validators.required]),
+      picture: new FormControl(''),
+      communicateMethod: new FormControl(''),
+      userMemo: new FormControl(''),
     });
     this.devicemodifyForm = new FormGroup({
-      'name': new FormControl("", [Validators.required]),
-      'customerName': new FormControl(null, [Validators.required]),
-      'model': new FormControl("",),
-      'location': new FormControl("", [Validators.required, Validators.maxLength(30)]),
-      'installDate': new FormControl("",),
-      'picture': new FormControl("",),
-      'communicateMethod': new FormControl("",),
-      'userMemo': new FormControl("",),
+      name: new FormControl(''),
+      customerName: new FormControl(null, [Validators.required]),
+      model: new FormControl(''),
+      location: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(30),
+      ]),
+      installDate: new FormControl(''),
+      picture: new FormControl(''),
+      communicateMethod: new FormControl(''),
+      userMemo: new FormControl(''),
     });
-
   }
 
   ngOnDestroy() {
@@ -147,43 +140,39 @@ export class DeviceComponent implements OnInit {
   }
 
   onFileChange(event): void {
-    this.fileSelected = event.target.files[0]
+    this.fileSelected = event.target.files[0];
     const formData = new FormData();
-    formData.append(
-      "file",
-      this.fileSelected
-    );
+    formData.append('file', this.fileSelected);
 
     this.service.uploadanal(formData).subscribe({
       next: (res) => {
         this.imageSrc = res.url;
         this.deviceenrollForm.patchValue({
-          picture: this.imageSrc
-        })
+          picture: this.imageSrc,
+        });
         this.devicemodifyForm.patchValue({
-          picture: this.imageSrc
-        })
+          picture: this.imageSrc,
+        });
       },
       error: (err) => {
-        alert('서버 에러메세지')
+        alert('서버 에러메세지');
       },
-      complete: () => {
-      }
+      complete: () => {},
     });
   }
 
   clickedModalClose() {
     this.modal = false;
-    this.deviceenrollForm.reset()
-    this.imageSrc = "";
+    this.deviceenrollForm.reset();
+    this.imageSrc = '';
   }
   clickedModal() {
     this.modal = true;
   }
   clickedModal2Close() {
-    this.deviceenrollForm.reset()
+    this.deviceenrollForm.reset();
     this.modal2 = false;
-    this.imageSrc = "";
+    this.imageSrc = '';
   }
   clickedModal2() {
     this.modal2 = true;
@@ -191,8 +180,8 @@ export class DeviceComponent implements OnInit {
 
   picturemodalopen(index) {
     this.picturemodal = true;
-    let picturesrc = this.getalldevicesdata[index].picture
-    this.bigpicturesrc = picturesrc
+    let picturesrc = this.getalldevicesdata[index].picture;
+    this.bigpicturesrc = picturesrc;
   }
   picturemodalclose() {
     this.picturemodal = false;
@@ -201,58 +190,55 @@ export class DeviceComponent implements OnInit {
   deviceenroll() {
     const data = this.deviceenrollForm.value;
     if (data.customerName === null) {
-      delete data.customerName
-      data.customerName = "";
+      delete data.customerName;
+      data.customerName = '';
     }
     if (data.deviceId === null) {
-      delete data.deviceId
-      data.deviceId = "";
+      delete data.deviceId;
+      data.deviceId = '';
     }
-    if (data.picture === "") {
-      data.picture = "http://api-2207.bs-soft.co.kr/api/images/noimage.png";
+    if (data.picture === '') {
+      data.picture = 'http://api-2207.bs-soft.co.kr/api/images/noimage.png';
     }
 
     if (this.deviceenrollForm.valid) {
-      console.log(data, 'llw')
+      console.log(data, 'llw');
       this.service.deviceenroll(data).subscribe({
         next: (res) => {
-          alert('디바이스 등록이 완료되었습니다')
-          this.deviceenrollForm.reset()
+          alert('디바이스 등록이 완료되었습니다');
+          this.deviceenrollForm.reset();
           this.getalldevices();
           this.modal = false;
         },
         error: (err) => {
-          alert('권한이 없습니다')
+          alert('권한이 없습니다');
         },
-        complete: () => {
-        }
+        complete: () => {},
       });
     } else {
-      alert('필수정보, 최대길이 제한을 지켜주세요')
+      alert('필수정보, 최대길이 제한을 지켜주세요');
     }
-
   }
 
   getonedeviceId;
   getonecustomerName;
   getOneDevice(index) {
     this.modal2 = true;
-    this.getonedevicedata = this.getalldevicesdata[index]
-    this.getonedeviceId = this.getonedevicedata['deviceId']
-    this.getonecustomerName = this.getonedevicedata['customerName']
-    this.imageSrc = this.getonedevicedata["picture"]
+    this.getonedevicedata = this.getalldevicesdata[index];
+    this.getonedeviceId = this.getonedevicedata['deviceId'];
+    this.getonecustomerName = this.getonedevicedata['customerName'];
+    this.imageSrc = this.getonedevicedata['picture'];
 
     this.devicemodifyForm.patchValue({
-      name: this.getonedevicedata["name"],
-      model: this.getonedevicedata["model"],
-      customerName: this.getonedevicedata["customerName"],
+      name: this.getonedevicedata['name'],
+      model: this.getonedevicedata['model'],
+      customerName: this.getonedevicedata['customerName'],
       picture: this.getonedevicedata['picture'],
-      location: this.getonedevicedata["location"],
-      installDate: this.getonedevicedata["installDate"],
-      communicateMethod: this.getonedevicedata["communicateMethod"],
-      userMemo: this.getonedevicedata["userMemo"],
-    })
-
+      location: this.getonedevicedata['location'],
+      installDate: this.getonedevicedata['installDate'],
+      communicateMethod: this.getonedevicedata['communicateMethod'],
+      userMemo: this.getonedevicedata['userMemo'],
+    });
   }
 
   modifyonedevice() {
@@ -261,57 +247,51 @@ export class DeviceComponent implements OnInit {
     if (this.devicemodifyForm.valid) {
       this.service.modifyonedevice(data).subscribe({
         next: (res) => {
-          alert('기기 정보 수정이 완료되었습니다')
-          this.getalldevices()
-          this.devicemodifyForm.reset()
+          alert('기기 정보 수정이 완료되었습니다');
+          this.getalldevices();
+          this.devicemodifyForm.reset();
           this.modal2 = false;
         },
         error: (err) => {
-          alert('정보를 잘못 입력하셨습니다')
+          alert('정보를 잘못 입력하셨습니다');
         },
-        complete: () => {
-        }
+        complete: () => {},
       });
     } else {
-      alert('필수정보, 최대길이 제한을 지켜주세요')
+      alert('필수정보, 최대길이 제한을 지켜주세요');
     }
   }
 
   deleteonedevice(index) {
-    const returnValue = confirm('기기 정보를 삭제 하시겠습니까?')
+    const returnValue = confirm('기기 정보를 삭제 하시겠습니까?');
 
     if (returnValue) {
-      const deviceid = this.getalldevicesdata[index]["deviceId"]
+      const deviceid = this.getalldevicesdata[index]['deviceId'];
       this.service.deleteonedevice(deviceid).subscribe({
         next: (res) => {
-          alert('기기 정보 삭제가 완료되었습니다')
-          this.getalldevices()
+          alert('기기 정보 삭제가 완료되었습니다');
+          this.getalldevices();
         },
         error: (err) => {
-          alert('기기 정보 삭제 실패')
+          alert('기기 정보 삭제 실패');
         },
-        complete: () => {
-        }
+        complete: () => {},
       });
     }
   }
 
   chkinspection(index) {
-    const data = [this.getalldevicesdata[index].deviceId, { "inspection": !this.getalldevicesdata[index].inspection }];
+    const data = [
+      this.getalldevicesdata[index].deviceId,
+      { inspection: !this.getalldevicesdata[index].inspection },
+    ];
 
     this.service.modifyinspection(data).subscribe({
       next: (res) => {
         this.getalldevices();
       },
-      error: (err) => {
-
-      },
-      complete: () => {
-      }
+      error: (err) => {},
+      complete: () => {},
     });
   }
-
-
-
-
 }
